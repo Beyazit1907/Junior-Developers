bc8f34ccd2542634c88bddfa6f410ca7d100ae5b
Beyazıt <63406732+Beyazit1907@users.noreply.github.com>
2024-05-06 18:25:18 +0300

    dw

--- YARISMA MEBRE/ANASAYFA/main.js
+++ YARISMA MEBRE/ANASAYFA/main.js
@@ -1,10 +1,30 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const menuBar = document.querySelector('.menuBar');
-    const sidebar = document.getElementById('sidebar');
+    // Menü çubuğu ikonu
+    var menuIcon = document.querySelector('.menuBar i');
+    
+    // Sidebar
+    var sidebar = document.querySelector('.sidebar');
+    
+    // Menü çubuğu ikonuna tıklama olayı
+    menuIcon.addEventListener('click', function() {
+        // Sidebar açık mı kontrol et
+        if (sidebar.classList.contains('open')) {
+            // Sidebar açık ise kapat
+            sidebar.classList.remove('open');
+        } else {
+            // Sidebar kapalı ise aç
+            sidebar.classList.add('open');
+        }
+    });
 
-    function toggleSidebar() {
-        sidebar.classList.toggle('open');
-    }
+    // Sayfanın herhangi bir yerine tıklama olayını dinleme
+    document.addEventListener('click', function(event) {
+        // Tıklanan hedef öğeyi alın
+        var targetElement = event.target;
 
-    menuBar.addEventListener('click', toggleSidebar);
+        // Sidebar açık ve tıklanan öğe ne sidebar ne de menuIcon ise sidebar'ı kapat
+        if (sidebar.classList.contains('open') && !sidebar.contains(targetElement) && !menuIcon.contains(targetElement)) {
+            sidebar.classList.remove('open');
+        }
+    });
 });
